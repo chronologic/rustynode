e884701bbc0b22e58a75e4ca01b67cbedaa5a893
lsaether <Lsaether@protonmail.com>
2018-05-24 10:53:23 +0300

    timenode scaffolding

---
+++ src/timenode.rs
@@ -0,0 +1,15 @@
+pub struct Timenode {
+    cache: Cache;
+    //
+}
+
+pub struct Cache {
+    // store - The addresses of the scheduled transactions that the timenode is watching
+    // polling - The address of the condtional transactions the timenode is polling
+}
+
+impl Timenode {
+    pub fn subscribe_to(address_of_eventEmitter: String) {
+        unimplemented!();
+    }
+}
