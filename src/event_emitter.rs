60b74dbacd8e62e6334c63d99697bc4d97e5bb53
lsaether <Lsaether@protonmail.com>
2018-05-30 11:56:03 +0300

    picking up the new event logs correctly

--- src/event_emitter.rs
+++ src/event_emitter.rs
@@ -28,17 +28,50 @@ impl Event_Emitter<WebSocket>
         }
     }
 
+    pub fn get_newTransactionScheduled(&self, block_num: u64) {
+        let filter = FilterBuilder::default()
+            .address(vec![self.instance.address()])
+            .topics(
+                // None,
+                Some(vec![
+                    "94c6f2d01cc82df9dceeabfd7786c57a01cd9796e7cab146d2d0cf5c8380310d".into(),
+                ]),
+                None,
+                None,
+                None,
+            )
+            .from_block(block_num.into())
+            .build();
+
+
+        let event_future = self.web3.eth_filter()
+            .create_logs_filter(filter)
+            .then(|filter| {
+                filter
+                    .unwrap()
+                    .stream(time::Duration::from_secs(0))
+                    .for_each(|log| {
+                        println!("got log: {:?}", log);
+                        Ok(())
+                    })
+            })
+            .map_err(|_| ());
+        
+        event_future.wait();
+    }
+
     pub fn watch_newTransactionScheduled(&self, scheduled_from: Address) {
         let filter = FilterBuilder::default()
             .address(vec![self.instance.address()])
             .topics(
                 Some(vec![
-                    "0x94c6f2d01cc82df9dceeabfd7786c57a01cd9796e7cab146d2d0cf5c8380310d".into(),
+                    "94c6f2d01cc82df9dceeabfd7786c57a01cd9796e7cab146d2d0cf5c8380310d".into(),
                 ]),
                 None,
-                Some(vec![
-                    scheduled_from.into(),
-                ]),
+                None,
+                // Some(vec![
+                //     scheduled_from.into(),
+                // ]),
                 None,
             )
             .build();
@@ -53,7 +86,9 @@ impl Event_Emitter<WebSocket>
                         Ok(())
                     })
             })
-            .map_err(|_| ());
+            .map_err(|e| println!("{:?}", e));
+    
+        event_future.wait();
     }
 
     pub fn works(&self) {
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use web3::types::H160;
 
 use web3::Web3;
 use web3::transports::WebSocket;
+use web3::futures::{Future, Stream};
+
+
+use std::{thread,time};
 
 fn main() {
 
@@ -15,8 +19,8 @@ fn main() {
     let (_eloop, transport) = WebSocket::new("ws://localhost:8546").unwrap();
     let web3 = Web3::new(transport);
 
-    let tn: Timenode<WebSocket> = Timenode::boot(web3.clone());
-    tn.works();
+    // let tn: Timenode<WebSocket> = Timenode::boot(web3.clone());
+    // tn.works();
 
     let e_e_address: H160 = "bf21760528357ea7ef3f1eaf5513fe5d495f19c4"
         .parse().unwrap();
@@ -26,5 +30,10 @@ fn main() {
         web3.clone(),
     );
 
-    e_e.works();
+    // e_e.get_newTransactionScheduled(7511598);
+
+    e_e.watch_newTransactionScheduled("fe3afc02954c4d989f572f61ee185d654c0c7134".parse().unwrap());
+
+    // thread::sleep(time::Duration::from_secs(10_000))
+
 }
